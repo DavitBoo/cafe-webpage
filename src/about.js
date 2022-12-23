1861b4c60e0b5e0fede11db955e0db86ecb7e52a
DavitBoo <davidmarras@gmail.com>
2022-12-23 16:35:01 +0100

    text background css changes

--- src/about.js
+++ src/about.js
@@ -3,10 +3,12 @@ const about = () => {
     element.setAttribute('id','about');
     element.classList.add('container')
     element.classList.add('hide')
-    element.innerHTML = `<h2>Let's talk</h2>
-    <h1>About us</h1>
-    <hr>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque placerat, tellus sit amet ultrices porttitor, ex ex aliquet purus, pellentesque rhoncus felis ipsum nec purus. Donec id enim in risus pellentesque blandit. Proin sed nibh nec arcu mollis rhoncus in sit amet augue. Suspendisse magna nisl, eleifend non tempor et, luctus id quam. Nulla bibendum vulputate ante, non malesuada eros lobortis euismod`
+    element.innerHTML = `<div class="flex-container">
+        <h2>Let's talk</h2>
+        <h1>About us</h1>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque placerat, tellus sit amet ultrices porttitor, ex ex aliquet purus, pellentesque rhoncus felis ipsum nec purus. Donec id enim in risus pellentesque blandit. Proin sed nibh nec arcu mollis rhoncus in sit amet augue. Suspendisse magna nisl, eleifend non tempor et, luctus id quam. Nulla bibendum vulputate ante, non malesuada eros lobortis euismod
+    </div>`
 
     return element
 }
--- src/container.js
+++ src/container.js
@@ -3,11 +3,13 @@ const container = () => {
     const element = document.createElement('div')
     element.setAttribute('id','home');
     element.classList.add('container')
-    element.innerHTML = `<h2>Welcome to,</h2>
-    <h1>La Cafetería</h1>
-    <hr>
-    <p>Café de especialidad, cerveza artesana, juegos de mesa, repostería casera ¿Te vienes?</p>
-    <button id="btn-menu">Ver el menú</button>
+    element.innerHTML = `<div class="flex-container">
+        <h2>Welcome to,</h2>
+        <h1>La Cafetería</h1>
+        <hr>
+        <p>Café de especialidad, cerveza artesana, juegos de mesa, repostería casera ¿Te vienes?</p>
+        <button id="btn-menu">Ver el menú</button>
+    </div>
     `
 
     return element
