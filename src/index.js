fb0b950f8b4145bc6cb4be117b60051987cd459f
DavitBoo <davidmarras@gmail.com>
2022-12-23 12:08:59 +0100

    css scroll bar

--- src/index.js
+++ src/index.js
@@ -48,7 +48,7 @@ function menu() {
     element.setAttribute('id','menu');
     element.classList.add('container')
     element.classList.add('hide')
-    element.innerHTML = `<h2>Welcome to,</h2>
+    element.innerHTML = `<h2>You can check,</h2>
     <h1>Our Menú</h1>
     <hr>
     <div class="menu-container">
@@ -102,8 +102,8 @@ function about() {
     element.setAttribute('id','about');
     element.classList.add('container')
     element.classList.add('hide')
-    element.innerHTML = `<h2>Welcome to,</h2>
-    <h1>About</h1>
+    element.innerHTML = `<h2>Let's talk</h2>
+    <h1>About us</h1>
     <hr>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque placerat, tellus sit amet ultrices porttitor, ex ex aliquet purus, pellentesque rhoncus felis ipsum nec purus. Donec id enim in risus pellentesque blandit. Proin sed nibh nec arcu mollis rhoncus in sit amet augue. Suspendisse magna nisl, eleifend non tempor et, luctus id quam. Nulla bibendum vulputate ante, non malesuada eros lobortis euismod`
 
